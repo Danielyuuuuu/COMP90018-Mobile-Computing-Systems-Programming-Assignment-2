7a53a627cb5f20568e03bd3021df583fd8b48ec3
Danielyuuuuu <48498200+Danielyuuuuu@users.noreply.github.com>
2021-09-23 17:52:57 +1000

    The map locates user's current location

--- app/src/main/java/com/example/dansdistractor/MainActivity.java
+++ app/src/main/java/com/example/dansdistractor/MainActivity.java
@@ -1,20 +1,47 @@
 package com.example.dansdistractor;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import java.util.List;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
 public class MainActivity extends AppCompatActivity {
+    public static final int DEFAULT_UPDATE_INTERVAL = 30;
+    public static final int FAST_UPDATE_INTERVAL = 5;
+    private static final int PERMISSION_FINE_LOCATION = 10;
 
     Button btn_map;
     MyApplication myApplication;
     List<Location> savedLocations;
+
+    // Current location
+    Location currentLocation;
+
+    // Location request is a config file for all settings related to FusedLocationProviderClient
+    LocationRequest locationRequest;
+
+    LocationCallback locationCallBack;
+
+    // Google's API for location services. The majority of the app functions using this class.
+    FusedLocationProviderClient fusedLocationProviderClient;
 
 
     @Override
@@ -26,10 +53,19 @@ public class MainActivity extends AppCompatActivity {
 
         myApplication = (MyApplication)getApplicationContext();
 
+        // Set all properties of LocationRequest
+        locationRequest = new LocationRequest();
+        locationRequest.setInterval(1000 * DEFAULT_UPDATE_INTERVAL);
+        locationRequest.setFastestInterval(1000 * FAST_UPDATE_INTERVAL);
+        locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+
+        // Request to access location permission from the user
+        requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_FINE_LOCATION);
+
         btn_map.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                generateRandomPoints(3);
+                updateGPS();
 
                 Intent i = new Intent(MainActivity.this, MapsActivity.class);
                 startActivity(i);
@@ -37,8 +73,47 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void generateRandomPoints(int num){
-        savedLocations = myApplication.getMyLocations();
 
+    private void updateGPS(){
+        // Get permissions from the user to track GPS
+        // Get the current location from the fused client
+        // Update the UI - i.e. set all properties in their associated text view items
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(MainActivity.this);
+
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            // User provided the permission
+            fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    // We got permission. Put the values of location. XXX into the UI components.
+//                    updateUIValues(location);
+                    currentLocation = location;
+                    savedLocations = myApplication.getMyLocations();
+                    savedLocations.add(currentLocation);
+                }
+            });
+        }
+        else{
+            // Permission not granted yet
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_FINE_LOCATION);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch(requestCode){
+            case PERMISSION_FINE_LOCATION:
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    updateGPS();
+                }
+                else{
+                    Toast.makeText(this, "This app requires to grant location permission to be able to work", Toast.LENGTH_LONG).show();
+                    finish();
+                }
+                break;
+        }
     }
 }
