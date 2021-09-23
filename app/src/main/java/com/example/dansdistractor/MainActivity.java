9227cdf65ba0fb2976ab51ef3757c9ab239c899d
Danielyuuuuu <48498200+Danielyuuuuu@users.noreply.github.com>
2021-09-23 13:47:18 +1000

    Created a MyApplication class that stores the session data

--- app/src/main/java/com/example/dansdistractor/MainActivity.java
+++ app/src/main/java/com/example/dansdistractor/MainActivity.java
@@ -3,13 +3,18 @@ package com.example.dansdistractor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     Button btn_map;
+    MyApplication myApplication;
+    List<Location> savedLocations;
 
 
     @Override
@@ -19,12 +24,21 @@ public class MainActivity extends AppCompatActivity {
 
         btn_map = findViewById(R.id.btn_map);
 
+        myApplication = (MyApplication)getApplicationContext();
+
         btn_map.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                generateRandomPoints(3);
+
                 Intent i = new Intent(MainActivity.this, MapsActivity.class);
                 startActivity(i);
             }
         });
+    }
+
+    private void generateRandomPoints(int num){
+        savedLocations = myApplication.getMyLocations();
+
     }
 }
--- app/src/main/java/com/example/dansdistractor/MapsActivity.java
+++ app/src/main/java/com/example/dansdistractor/MapsActivity.java
@@ -1,21 +1,29 @@
 package com.example.dansdistractor;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
 
+import android.location.Location;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.dansdistractor.databinding.ActivityMapsBinding;
+
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
+
+    List<Location> savedLocations;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +36,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        MyApplication myApplication = (MyApplication)getApplicationContext();
+        savedLocations = myApplication.getMyLocations();
     }
 
     /**
@@ -44,8 +55,37 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+//        LatLng sydney = new LatLng(-34, 151);
+//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        LatLng lastLocationPlaced = new LatLng(-34, 151);
+
+        for(Location location: savedLocations){
+            LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+            MarkerOptions markerOptions = new MarkerOptions();
+            markerOptions.position(latLng);
+            markerOptions.title("Lat: " + location.getLatitude() + "; Lon: " + location.getLongitude());
+            mMap.addMarker(markerOptions);
+            lastLocationPlaced = latLng;
+        }
+
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(lastLocationPlaced, 12));
+
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(@NonNull Marker marker) {
+                // lets count the number of times that pin is clicked
+                Integer clicks = (Integer) marker.getTag();
+                if(clicks == null){
+                    clicks = 0;
+                }
+                clicks++;
+                marker.setTag(clicks);
+                Toast.makeText(MapsActivity.this, "Marker " + marker.getTitle() + " was clicked " + marker.getTag(), Toast.LENGTH_SHORT).show();
+
+                return false;
+            }
+        });
     }
 }
