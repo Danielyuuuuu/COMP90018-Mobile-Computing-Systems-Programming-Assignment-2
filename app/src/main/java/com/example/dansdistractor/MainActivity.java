4e93a9dfd8e4ee4fa721ebdc6d556d674a342f93
Danielyuuuuu <48498200+Danielyuuuuu@users.noreply.github.com>
2021-09-23 13:14:58 +1000

    Added MapsActivity

--- app/src/main/java/com/example/dansdistractor/MainActivity.java
+++ app/src/main/java/com/example/dansdistractor/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.dansdistractor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_map;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_map = findViewById(R.id.btn_map);
+
+        btn_map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, MapsActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
