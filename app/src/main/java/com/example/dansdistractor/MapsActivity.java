e8fe96cc4aa65647b335f84880c4d1b3d98af3de
Danielyuuuuu <48498200+Danielyuuuuu@users.noreply.github.com>
2021-09-25 13:08:06 +1000

    Removing location updates when the user has closed the session

--- app/src/main/java/com/example/dansdistractor/MapsActivity.java
+++ app/src/main/java/com/example/dansdistractor/MapsActivity.java
@@ -263,6 +263,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
+                fusedLocationProviderClient.removeLocationUpdates(locationCallBack);
                 finish();
             }
         });
