fb4876a8cf8d9db5a0a99c9589a3a2f7fa762d94
Ray Chen <46772434+rayjtchen@users.noreply.github.com>
2021-10-09 02:26:28 +1100

    Add messageSchema

---
+++ app/src/main/java/com/example/dansdistractor/databaseSchema/MessageSchema.java
@@ -0,0 +1,16 @@
+package com.example.dansdistractor.databaseSchema;
+
+import java.util.Date;
+
+public class MessageSchema {
+
+    public String author, lat, lon, content;
+    public Date date;
+
+    public MessageSchema() {
+    }
+
+    public MessageSchema(String name, String email) {
+
+    }
+}
