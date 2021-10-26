ca2aa48e9d7c6fe0b874e7ccce6e833d133711a7
Ray Chen <46772434+rayjtchen@users.noreply.github.com>
2021-10-26 15:56:10 +1100

    Update UserHistorySchema.java

--- app/src/main/java/com/example/dansdistractor/UserHistorySchema.java
+++ app/src/main/java/com/example/dansdistractor/UserHistorySchema.java
@@ -1,25 +1,64 @@
 package com.example.dansdistractor;
 
+import android.location.Location;
+
+import com.google.android.gms.maps.model.LatLng;
+
 import java.sql.Time;
 import java.util.Date;
+import java.util.List;
 
 public class UserHistorySchema {
 
-    public Date date;
-    public Time startTime, endTime;
-    public int distance, steps, avgSpeed, pins, points;
+    public Date endDateTime, startDateTime;
+    public int steps, pins;
+    public double distance, avgSpeed;
+
+    public List<Location> myLocations = null;
+    private List<LatLng> targetLocations = null;
+    private List<LatLng> completedTargetLocations = null;
 
     public UserHistorySchema() {
     }
 
-    public UserHistorySchema(Date date, Time startTime) {
-        this.date = date;
-        this.startTime = startTime;
-        this.endTime = null;
-        this.distance = 0;
-        this.steps = 0;
-        this.avgSpeed = 0;
-        this.pins = 0;
-        this.points = 0;
+    public UserHistorySchema(Date endDateTime, Date startDateTime, List<Location> myLocations, List<LatLng> targetLocations, List<LatLng> completedTargetLocations, int steps) {
+        this.endDateTime = endDateTime;
+        this.startDateTime = startDateTime;
+        this.myLocations = myLocations;
+        this.targetLocations = targetLocations;
+        this.completedTargetLocations = completedTargetLocations;
+        this.steps = steps;
+        this.distance = getDistance();
+        this.avgSpeed = getSpeed();
+        this.pins = getPins();
     }
+
+    //get distance in meters between two location instance
+    private double getDistance(){
+
+        double totalDistance = 0;
+
+        for (int counter = 0; counter < myLocations.size()-1; counter++) {
+
+            totalDistance += myLocations.get(counter).distanceTo(myLocations.get(counter+1));
+        }
+
+        return totalDistance;
+    }
+
+    private double getSpeed(){
+        if (endDateTime.getTime() - startDateTime.getTime() == 0) return 0;
+        return distance/endDateTime.getTime() - startDateTime.getTime();
+    }
+
+    private int getPins(){
+        return completedTargetLocations.size();
+    }
+
+
+
+
+
+
+
 }
