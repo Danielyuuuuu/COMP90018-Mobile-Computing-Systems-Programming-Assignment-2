c547e17f80767e58e80d911bff66700febf7be16
Ray Chen <46772434+rayjtchen@users.noreply.github.com>
2021-10-27 17:04:42 +1100

    Add close button function to summary page

--- app/src/main/java/com/example/dansdistractor/summary.java
+++ app/src/main/java/com/example/dansdistractor/summary.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class summary extends AppCompatActivity {
     private RingProgressView ringProgressView;
     int myProgress=0;
+    private Button btn_close;
+
+
 //    int mySteps=210;
 //    double myMileage=1.2;
 //    int myDuration=50;
@@ -16,10 +21,21 @@ public class summary extends AppCompatActivity {
 //    int myCalorie=1200;
 //    int myPoint=500;
 //    int myVoucher=3;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_summary);
+
+        btn_close = findViewById(R.id.btn_close);
+
+        btn_close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(summary.this, MainActivity.class));
+            }
+        });
 
         Intent intent =getIntent();
 
