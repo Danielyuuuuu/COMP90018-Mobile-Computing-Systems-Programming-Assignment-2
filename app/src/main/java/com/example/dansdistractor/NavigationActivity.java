ea55b6bf3aa1e03dece2d25c4f811ce46c7d435a
zhenhaochen <xcorrnner@gmail.com>
2021-10-27 13:28:15 +0800

    create main page fragment and fix some bug

--- app/src/main/java/com/example/dansdistractor/NavigationActivity.java
+++ app/src/main/java/com/example/dansdistractor/NavigationActivity.java
@@ -3,10 +3,16 @@ package com.example.dansdistractor;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.dansdistractor.databaseSchema.UserSchema;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -15,29 +21,33 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.dansdistractor.databinding.ActivityNavigationBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class NavigationActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityNavigationBinding binding;
 
+    // get user info from database
+    private FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    private String userID = user.getUid();
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private DocumentReference docRefCurrentUser = db.collection("Users").document(userID);
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         binding = ActivityNavigationBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
         setSupportActionBar(binding.appBarNavigation.toolbar);
-//        binding.appBarNavigation.fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
@@ -47,6 +57,34 @@ public class NavigationActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_navigation);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        View header = navigationView.getHeaderView(0);
+        final TextView username = (TextView) header.findViewById(R.id.username);
+        final TextView useremail = (TextView) header.findViewById(R.id.useremail);
+
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null) {
+            docRefCurrentUser.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                @Override
+                public void onSuccess(@NonNull DocumentSnapshot documentSnapshot) {
+                    UserSchema userProfile = documentSnapshot.toObject(UserSchema.class);
+
+                    if (userProfile != null) {
+                        String nameUserProfile = userProfile.name;
+                        String emailUserProfile = userProfile.email;
+
+                        username.setText(nameUserProfile);
+                        useremail.setText(emailUserProfile);
+                    }
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(NavigationActivity.this, "Unable to obtain user info", Toast.LENGTH_LONG).show();
+                }
+            });
+        }
     }
 
     @Override
